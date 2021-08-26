a55535f5eec7cb68a193ea5064696e5dfe011208
William Venner <william@venner.io>
2021-08-27 00:15:30 +0100

    fix 32 bit

--- src/bindings.rs
+++ src/bindings.rs
@@ -2,12 +2,16 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", target_pointer_width = "64"))]
 pub type size_t = ::std::os::raw::c_ulong;
 
-#[cfg(target_os = "windows")]
+#[cfg(all(target_os = "windows", target_pointer_width = "64"))]
 pub type size_t = ::std::os::raw::c_ulonglong;
 
+#[cfg(target_pointer_width = "32")]
+pub type size_t = ::std::os::raw::c_uint;
+
+#[cfg(not(all(target_os = "windows", target_pointer_width = "32")))]
 extern "C" {
 	pub fn LzmaCompress(
 		dest: *mut ::std::os::raw::c_uchar,
@@ -24,8 +28,35 @@ extern "C" {
 		fb: ::std::os::raw::c_int,
 		numThreads: ::std::os::raw::c_int,
 	) -> ::std::os::raw::c_int;
+
+	pub fn LzmaUncompress(
+		dest: *mut ::std::os::raw::c_uchar,
+		destLen: *mut size_t,
+		src: *const ::std::os::raw::c_uchar,
+		srcLen: *mut size_t,
+		props: *const ::std::os::raw::c_uchar,
+		propsSize: size_t,
+	) -> ::std::os::raw::c_int;
 }
-extern "C" {
+
+#[cfg(all(target_os = "windows", target_pointer_width = "32"))]
+extern "stdcall" {
+	pub fn LzmaCompress(
+		dest: *mut ::std::os::raw::c_uchar,
+		destLen: *mut size_t,
+		src: *const ::std::os::raw::c_uchar,
+		srcLen: size_t,
+		outProps: *mut ::std::os::raw::c_uchar,
+		outPropsSize: *mut size_t,
+		level: ::std::os::raw::c_int,
+		dictSize: ::std::os::raw::c_uint,
+		lc: ::std::os::raw::c_int,
+		lp: ::std::os::raw::c_int,
+		pb: ::std::os::raw::c_int,
+		fb: ::std::os::raw::c_int,
+		numThreads: ::std::os::raw::c_int,
+	) -> ::std::os::raw::c_int;
+
 	pub fn LzmaUncompress(
 		dest: *mut ::std::os::raw::c_uchar,
 		destLen: *mut size_t,
