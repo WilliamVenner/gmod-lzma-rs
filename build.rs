7bb7e3541947a757757df92a238ef8022cb4c143
William Venner <william@venner.io>
2021-08-26 23:55:19 +0100

    remove bindgen dependency and use static bindings

--- build.rs
+++ build.rs
@@ -10,20 +10,4 @@ fn main() {
 		.file("bootil/src/3rdParty/lzma/LzmaEnc.c")
 		.file("bootil/src/3rdParty/lzma/Alloc.c")
 		.compile("lzma");
-
-	// Generate bindings
-	let bindings = bindgen::Builder::default()
-		.header("bootil/src/3rdParty/lzma/Types.h")
-		.header("bootil/src/3rdParty/lzma/LzmaLib.h")
-		.allowlist_function("LzmaCompress")
-		.allowlist_function("LzmaUncompress")
-		.default_macro_constant_type(bindgen::MacroTypeVariation::Signed)
-		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
-		.generate()
-		.expect("Unable to generate bindings");
-
-	let out_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
-	bindings
-		.write_to_file(out_path.join("bindings.rs"))
-		.expect("Couldn't write bindings!");
 }
--- src/bindings.rs
+++ src/bindings.rs
@@ -2,4 +2,36 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[cfg(target_os = "linux")]
+pub type size_t = ::std::os::raw::c_ulong;
+
+#[cfg(target_os = "windows")]
+pub type size_t = ::std::os::raw::c_ulonglong;
+
+extern "C" {
+	pub fn LzmaCompress(
+		dest: *mut ::std::os::raw::c_uchar,
+		destLen: *mut size_t,
+		src: *const ::std::os::raw::c_uchar,
+		srcLen: size_t,
+		outProps: *mut ::std::os::raw::c_uchar,
+		outPropsSize: *mut size_t,
+		level: ::std::os::raw::c_int,
+		dictSize: ::std::os::raw::c_uint,
+		lc: ::std::os::raw::c_int,
+		lp: ::std::os::raw::c_int,
+		pb: ::std::os::raw::c_int,
+		fb: ::std::os::raw::c_int,
+		numThreads: ::std::os::raw::c_int,
+	) -> ::std::os::raw::c_int;
+}
+extern "C" {
+	pub fn LzmaUncompress(
+		dest: *mut ::std::os::raw::c_uchar,
+		destLen: *mut size_t,
+		src: *const ::std::os::raw::c_uchar,
+		srcLen: *mut size_t,
+		props: *const ::std::os::raw::c_uchar,
+		propsSize: size_t,
+	) -> ::std::os::raw::c_int;
+}
